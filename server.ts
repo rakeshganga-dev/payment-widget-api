a1d7ff08d210353f3ccf757bf838cb8019653a7d
Rakesh Ganga <rakeshganga012@gmail.com>
2025-05-03 16:49:06 +0530

    added cors

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
